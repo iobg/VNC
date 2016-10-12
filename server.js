fe31943ebba1c22d1679adacd8af73774a026372
Banks Humphreys <iobg0320@gmail.com>
2016-10-12 17:54:54 -0500

    added more dependancies

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const { Server }= require('http')
 const server = Server(app)
 const socketio = require('socket.io')
+const ss = require('socket.io-stream')
 const io = socketio(server)
 
 
@@ -15,10 +16,35 @@ app.get('/',(req,res)=>{
 	res.render('index')
 })
 
-io.on('connect',socket=>{
-	console.log(`socket connected ${socket.id}`)
-})
 
 server.listen(3000,()=>{
 	console.log('server listening')
 })
+
+
+//screenshot
+const fs = require('fs')
+var exec = require('child_process').exec;
+
+const getScreenshot=()=>{
+	return new Promise((resolve,reject)=>{
+		exec('screencapture screenshot.png -C', function (err){
+			if(err){
+				console.log(err)
+				reject()
+						}
+						resolve()
+					})
+
+				})
+			}
+
+getScreenshot().then(()=>{
+	let read = fs.createReadStream('screenshot.png')
+		read.pipe(process.stdout)
+})
+
+io.on('connect',socket=>{
+	console.log(`socket connected ${socket.id}`)
+})
+
