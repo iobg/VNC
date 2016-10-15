49bc4b088b2c7403efa19944bf8ccb43550a544a
Banks Humphreys <iobg0320@gmail.com>
2016-10-14 20:18:11 -0500

    added GET route for the video

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const { Server }= require('http')
 const server = Server(app)
 const socketio = require('socket.io')
 var stdin = process.openStdin()
+var total = 999999999         // fake a large file
+var partialstart = 0
+var partialend = total - 1
+
+
+
+var start = parseInt(partialstart, 10); 
+var end = partialend ? parseInt(partialend, 10) : total;  
+
+var chunksize = (end-start)+1; 
+
+
 
 
 
@@ -12,16 +24,31 @@ var stdin = process.openStdin()
 app.set('view engine', 'pug')
 //middleware
 app.use(express.static('public'))
+
 app.get('/',(req,res)=>{
 	res.render('index')
+
+
+})
+
+
+app.get('/movie.avi',(req,res)=>{
+	res.writeHead(206, {
+    'Transfer-Encoding': 'chunked'
+   , 'Content-Type': 'video/avi'
+   , 'Content-Length': chunksize
+   , 'Accept-Ranges': 'bytes ' + start + "-" + end + "/" + total
+	});
+	stdin.on('data', chunk=>{
+		stdin.pipe(res)
+})
+
 })
 
 
 server.listen(3000,()=>{
 	console.log('server listening')
-		stdin.on('data', chunk=>{
-		console.log(chunk)
-})
+		
 })
 
 
