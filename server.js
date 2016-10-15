9adb3daa66add88291449a30abc4754ee85afb1a
Banks Humphreys <iobg0320@gmail.com>
2016-10-14 19:23:23 -0500

    scrapped screenshot stuff, using ffmpeg for video recording to buffer

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const app = express()
 const { Server }= require('http')
 const server = Server(app)
 const socketio = require('socket.io')
-const ss = require('socket.io-stream')
-const io = socketio(server)
-const sharp = require('sharp')
+var stdin = process.openStdin()
+
 
 
 //server config
@@ -20,39 +19,10 @@ app.get('/',(req,res)=>{
 
 server.listen(3000,()=>{
 	console.log('server listening')
+		stdin.on('data', chunk=>{
+		console.log(chunk)
+})
 })
 
 
-//screenshot
-const fs = require('fs')
-var exec = require('child_process').exec;
-let currentFile=0
-
-const getScreenshot=()=>{
-	return new Promise((resolve,reject)=>{
-		// fs.unlink(`./images/screenshot.jpg`,()=>{
-		exec(`screencapture ./images/screenshot.jpg -C`, function (err){
-			if(err){
-				console.log(err)
-				reject()
-						}
-						resolve()
-					})
-				// })
-				})
-			}
-
-let i=0;
-const recordScreenShots=()=>{
-	getScreenshot().then(()=>{
-		let read=fs.createReadStream(`./images/screenshot.jpg`)
-		.pipe(sharp().resize(640,400).pipe(process.stdout))
-					recordScreenShots()
-					console.log(i++)		
-	})
-}
-recordScreenShots()
-io.on('connect',socket=>{
-	console.log(`socket connected ${socket.id}`)
-})
 
