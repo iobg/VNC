52eee660c6887d20d7f2e331e2fd9f85fb55c3c4
Banks Humphreys <iobg0320@gmail.com>
2016-10-20 10:41:32 -0500

    spawning ffmpeg process within node server

--- server.js
+++ server.js
@@ -7,11 +7,21 @@ const socketio = require('socket.io')
 const io = socketio(server)
 const fs = require('fs')
 const robot = require('robotjs')
-
+const { spawn } = require('child_process')
 const cors = require('cors')
 
 //middleware
+let args = ['-r','24','-f','avfoundation',
+					 '-capture_cursor','1','-i','1',
+					  '-vf','scale=1280:800','-preset','ultrafast',
+					  '-tune','zerolatency','-g','0', 
+					  '-hls_flags','delete_segments','-f','hls',
+					  '-hls_time','0.5','-hls_list_size','2', 
+					  '-hls_allow_cache','0','-hls_segment_filename',
+					  'videostream/file%03d.ts','videostream/stream.m3u8']
 
+let recording= spawn('ffmpeg', args )
+console.log('video recording')
 app.use(express.static('public'));
 app.use(cors())
 
@@ -24,7 +34,6 @@ app.get('/stream.m3u8',(req,res)=>{
 	fs.createReadStream('videostream/stream.m3u8').pipe(res)
 })
 app.get('/:streamSegment',(req,res)=>{
-	console.log('segment loading')
 		let read=fs.createReadStream(`videostream/${req.params.streamSegment}`)
 		read.on('error',console.log)
 		read.pipe(res)
