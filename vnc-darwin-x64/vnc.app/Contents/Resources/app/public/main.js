2b214a0816d26b20ca37041f6ab834cc3ee02781
Banks Humphreys <iobg0320@gmail.com>
2016-10-20 12:44:02 -0500

    everything running in electron

---
+++ vnc-darwin-x64/vnc.app/Contents/Resources/app/public/main.js
@@ -0,0 +1,26 @@
+const socket = io()
+const video = document.getElementById('video');
+
+  if(Hls.isSupported()) {
+    var hls = new Hls({capLevelToPlayerSize:true});
+    hls.loadSource('/stream.m3u8');
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+      video.play();
+  });
+ }
+
+video.addEventListener('click',()=>{
+	socket.emit('clientMouseClick')
+})
+
+video.addEventListener('mousemove',event=>{
+	mouseObj = {}
+	mouseObj.x = event.layerX
+	mouseObj.y = event.layerY
+	socket.emit('clientMouseMove',mouseObj)
+})
+window.addEventListener('keydown',event=>{
+  socket.emit('clientKeyPress',event.key)
+  })
+
