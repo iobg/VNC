3e5183be0e60fc3975209a04ce9be9573aa00352
Banks Humphreys <iobg0320@gmail.com>
2016-10-31 10:01:25 -0500

    message displayed to show current state of connection

--- electron/renderer.js
+++ electron/renderer.js
@@ -1,6 +1,7 @@
 const startBtn = document.getElementById('startBtn')
 const endBtn = document.getElementById('endBtn')
 const passwordGiven = document.getElementById('password')
+const status = document.getElementById('status')
 
 const {startServer,startRecording,endRecording,password} = require('../server')
 passwordGiven.innerText=`Password: ${password}`
@@ -11,16 +12,19 @@ startBtn.addEventListener('click',()=>{
 	if(!currentlyRecording){
 		startServer()
   	startRecording()
+  	status.innerText = "Connection Open"
+  	status.className = "start"
   	currentlyRecording=true;
   }
 })
 	
- 
 
 endBtn.addEventListener('click',()=>{
 	if(currentlyRecording){
 		endRecording()
 		currentlyRecording=false
+		status.innerText = "Connection Closed"
+		status.className = "end"
 		console.log('end')
 	}
 	
